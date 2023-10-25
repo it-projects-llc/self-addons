d24aa9dd9fbdcca788ebd22b16a233dab6c109e5
Eugene Molotov <molotov@it-projects.info>
2023-10-25 10:05:42 +0500

    [ADD] itpp_mail: добавил пустой модуль для модификации mail

---
+++ itpp_mail/README.rst
@@ -0,0 +1,16 @@
+===========================================
+ Discuss modifications for IT-Projects LLC
+===========================================
+
+Credits
+=======
+
+Contributors
+------------
+
+* `Eugene Molotov <https://github.com/em230418>`__
+
+Maintainers
+-----------
+
+* `IT-Projects LLC <https://it-projects.info>`__
---
+++ itpp_mail/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": """Discuss modifications for IT-Projects LLC""",
+    "version": "15.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/self-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "mail",
+    ],
+    "data": [],
+    "demo": [],
+}
