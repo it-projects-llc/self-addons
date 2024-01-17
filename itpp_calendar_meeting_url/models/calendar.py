210314e8d647f9d99052c1eb4dc38dc301f9cdfc
Ilmir Karamov <ilmir.karamov@gmail.com>
2024-01-17 19:15:59 +0500

    :ambulance: pre-commit issues

--- itpp_calendar_meeting_url/models/calendar.py
+++ itpp_calendar_meeting_url/models/calendar.py
@@ -1,21 +1,23 @@
-from odoo import api, fields, models
 import pytz
+
+from odoo import api, fields, models
 
 
 class Calendar(models.Model):
     _inherit = "calendar.event"
 
-    videocall_location = fields.Char(compute="_meeting_url")
+    videocall_location = fields.Char(compute="_compute_meeting_url")
 
     @api.depends("start")
-    def _meeting_url(self):
+    def _compute_meeting_url(self):
         for rec in self:
             rec.date_combination = rec.start.astimezone(
-                tz=pytz.timezone('Asia/Yekaterinburg')
+                tz=pytz.timezone("Asia/Yekaterinburg")
             ).strftime(
-                '%Y%m%d'
+                "%Y%m%d"
             )  # timezone(self.env.user.tz or 'Asia/Yekaterinburg').
-            # list of timezones https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568
+            # list of timezones:
+            # https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568
             rec.videocall_location = (
                 "https://meet.jit.si/discussion_%s" % rec.date_combination
             )
